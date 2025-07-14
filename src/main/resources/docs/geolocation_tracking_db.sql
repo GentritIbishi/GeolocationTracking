998307d5e79760d3f14ce132a7ccfedd5b7b3a07
gentritibishi <gentritibishi@gmail.com>
2025-07-14 10:09:48 +0200

    Version 1.3

--- src/main/resources/docs/geolocation_tracking_db.sql
+++ src/main/resources/docs/geolocation_tracking_db.sql
@@ -17,3 +17,6 @@ CREATE TABLE video_url (
 );
 
 INSERT INTO video_url (id, url) VALUES (1, 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+UPDATE video_url
+SET url = 'https://www.tiktok.com/@edits.xlyr1cs/video/7526343943508856120?lang=en&q=don%20xhoni&t=1752420336565'
+WHERE id = 1;
